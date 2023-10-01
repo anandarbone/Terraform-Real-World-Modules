1ff6bdaf5e1147018efe35fb6e6e991ad0ac1356
Anand.Arbone <anand.arbone@axpo.com>
2023-10-01 14:17:46 +0200

    username changed

--- 03-Azure/02-Resource-Group/C-Frontend/rg.tf
+++ 03-Azure/02-Resource-Group/C-Frontend/rg.tf
@@ -1,6 +1,6 @@
 module "axa_rg" {
-  source                  = "../B-Backend/"  # Path to your module directory
+  source    = "../B-Backend/"  # Path to your module directory
 
-  name     = var.rg_name
-  location = var.location
+  name      = var.rg_name
+  location  = var.location
 }
--- 03-Azure/02-Resource-Group/E-Frontend-Dynamic/rg.tf
+++ 03-Azure/02-Resource-Group/E-Frontend-Dynamic/rg.tf
@@ -1,5 +1,5 @@
 module "axa_rg" {
-  source                  = "../D-Backend-Dynamic/"  # Path to your module directory
+  source    = "../D-Backend-Dynamic/"  # Path to your module directory
 
   names     = var.rg_names
   location  = var.location
